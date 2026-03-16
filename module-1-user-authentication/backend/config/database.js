14239deadf262b99d0a4c682f43b7292274e8c6b
Abhishek <abhishek@example.com>
2026-03-16 13:03:08 +0530

    add URI logging for debug

--- module-1-user-authentication/backend/config/database.js
+++ module-1-user-authentication/backend/config/database.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const MONGO_URI = process.env.MONGODB_URI || 'mongodb+srv://findmystay:findmystay@findmystay.stufssv.mongodb.net/findmystay?retryWrites=true&w=majority&appName=findmystay';
+    console.log('Connecting to:', MONGO_URI.substring(0, 50));
     await mongoose.connect(MONGO_URI);
     console.log('MongoDB Connected');
   } catch (error) {
